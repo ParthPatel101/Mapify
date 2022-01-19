eb131d0f5fbdc4e03363ced45fb0e43129c4c544
Parth Patel <parthp1217@gmail.com>
2022-01-19 05:19:37 -0500

    Initial Commit

---
+++ Mapify/MapifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapifyApp.swift
+//  Mapify
+//
+//  Created by Parth Patel on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MapifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
